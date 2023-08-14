75dad644243f51b66ce82cf957b8f5c43ce7a7a8
Ajsensai <Ajsensairunescape@gmail.com>
2023-08-15 09:41:01 +1000

    updated foo.c

--- week4/foo.c
+++ week4/foo.c
@@ -1,1 +1,18 @@
 # include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    int count;
+    printf ("This was called with \"%s\".\n", argv[0]);
+    if (argc > 1)
+    {
+        for (i=1; i<argc; i++){
+            printf("argv[%d] = %s\n", i, argv[i]);
+        }
+    }
+    else
+    {
+        printf("The command has no other arguments.\n")
+    }
+    return 0;
+}

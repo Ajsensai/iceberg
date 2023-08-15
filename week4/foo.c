d2b97261b10a675d3d83a2c5380c5126585df3db
Ajsensai <Ajsensairunescape@gmail.com>
2023-08-15 10:29:49 +1000

    updated upperLower

--- week4/foo.c
+++ week4/foo.c
@@ -5,6 +5,18 @@ int usage(){
     printf("Arguments:\n -u Upper\n -l Lower\n");
 }
 
+void upperLower(char *option, char *text){
+    while (*text != '\0'){
+        if (*option == 'u'){
+            *text = (*text >= 'a' && *text <= 'z') ? (*text - 'a' + 'A') : *text;
+        }
+        else if (*option == 'l'){
+            *text = (*text >= 'A' && *text <= 'Z') ? (*text - 'A' + 'a') : *text;
+        }
+        text++;
+    }
+}
+
 int main (int argc, char *argv[]){
     if (argc != 3){
         usage();
@@ -18,5 +30,20 @@ int main (int argc, char *argv[]){
         return 1;
     }
     printf("And?....");
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char *text = (char *)malloc(file_size + 1);
+    if (text == NULL){
+        printf("Memory allocation error. \n");
+        fclose(file);
+        return 1;
+    }
+    fread(text, 1, file_size, file);
+    text[file_size] = '\0';
+    fclose(file);
+    upperLower(option, text);
+    printf("%s\n", text);
+    free(text);
     return 0;
 }
